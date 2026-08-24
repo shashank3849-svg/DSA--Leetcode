e284d585634a6036f5e80737782f814a6944acd2
shashank3849-svg <shashank.3849@gmail.com>
2026-08-24 19:57:43 +0530

    Time: 47 ms (5.36%) | Memory: 22.7 MB (18.59%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+// Remove Duplicates from Sorted Array
+//BY SHASHANK 
+// 24-08-26
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(int i= 1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]){
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+        }
+
+        return nums.size();
+        
+    }
+};
