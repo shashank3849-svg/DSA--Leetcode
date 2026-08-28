f36e1c14bef018a53e193d1d1e5d46e794f6fd68
shashank3849-svg <shashank.3849@gmail.com>
2026-08-28 13:53:22 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (12.38%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,17 @@
+//BY SHASHANK-
+//REMOVE ELEMENT, 28-08-26
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==val){
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+        }
+
+        return nums.size();
+        
+    }
+};
