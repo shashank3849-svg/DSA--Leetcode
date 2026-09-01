0a50bab7f097582fd2d26923136f2272b03fe7e3
shashank3849-svg <shashank.3849@gmail.com>
2026-09-01 22:55:57 +0530

    Time: 0 ms (100.00%) | Memory: 77.2 MB (46.08%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        ListNode* l3 = new ListNode(0);
+        ListNode* ptr = l3;
+
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry != 0) {
+
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            ptr->next = new ListNode(sum % 10);
+            carry = sum / 10;
+
+            ptr = ptr->next;
+        }
+
+        return l3->next;
+    }
+};
