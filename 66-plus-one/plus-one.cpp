2609430155947207b4848664e852268051dae9b4
shashank3849-svg <shashank.3849@gmail.com>
2026-09-03 21:20:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (26.36%) - LeetSync

---
+++ 66-plus-one/plus-one.cpp
@@ -0,0 +1,23 @@
+//By Shashank
+// Plus One , 03-09-2026
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int i= digits.size()-1;
+        while(i>= 0 &&digits[i]==9){
+            digits[i]=0;
+            i--;
+        }
+
+        if(i<0){
+            digits.insert(digits.begin(),1);
+        }
+        else{
+            digits[i]++;
+        }
+
+        return digits;
+        
+    }
+};
